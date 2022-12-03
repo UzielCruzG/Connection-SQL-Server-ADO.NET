bdfe6d09a0e879fe75f524a124afcaa17beedd18
willy <lrs19110493@purisima.tecnm.mx>
2022-12-02 18:54:52 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace ProyectoV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 404);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(112, 34);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "simpleButton1";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(144, 404);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(112, 34);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "simpleButton2";
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(264, 404);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(112, 34);
+            this.simpleButton3.TabIndex = 2;
+            this.simpleButton3.Text = "simpleButton3";
+            // 
+            // simpleButton4
+            // 
+            this.simpleButton4.Location = new System.Drawing.Point(382, 404);
+            this.simpleButton4.Name = "simpleButton4";
+            this.simpleButton4.Size = new System.Drawing.Size(112, 34);
+            this.simpleButton4.TabIndex = 3;
+            this.simpleButton4.Text = "simpleButton4";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 386);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.simpleButton4);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton simpleButton4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            //AGREGAR
+            try
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+                //var nombre = "prueba addTransaction";
+                //var descripcion = "descripcion";
+
+                //var query = @"select * from Areas where idArea = @idArea";
+
+                using (SqlConnection sql = new SqlConnection(connectionString))
+                {
+                    sql.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("stp_areas_add", sql))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        //cmd.Parameters.Add(new SqlParameter("@idArea", idArea));
+                        cmd.Parameters.Add(new SqlParameter("@nombre", "Willy Nuevo"));//evitar la vunerabilidad sql injection vidio 5
+                        cmd.Parameters.Add(new SqlParameter("@descripcion", "Willy Nuevo"));
+                        cmd.ExecuteNonQuery();
+                        //DataTable dt = new DataTable();
+                        //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        //sql.Open();
+
+                        //da.Fill(dt);
+                        //dataGridView1.DataSource = dt;
+                        //SqlDataReader lector;
+                        //lector = cmd.ExecuteReader();
+                    }
+
+
+
+                    var transaction = sql.BeginTransaction();
+                    using (SqlCommand cmd = new SqlCommand("stp_areas_add", sql, transaction))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        //cmd.Parameters.Add(new SqlParameter("@idArea", idArea));
+                        cmd.Parameters.Add(new SqlParameter("@nombre", "Willy9"));//evitar la vunerabilidad sql injection vidio 5
+                        cmd.Parameters.Add(new SqlParameter("@descripcion", "Willy9"));
+                        cmd.ExecuteNonQuery();
+                        //DataTable dt = new DataTable();
+                        //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        //sql.Open();
+
+                        //da.Fill(dt);
+                        //dataGridView1.DataSource = dt;
+                        //SqlDataReader lector;
+                        //lector = cmd.ExecuteReader();
+                    }
+                    //throw new ApplicationException("Ooops Error"); //Proboca el error en la transaccion
+
+
+                    using (SqlCommand cmd = new SqlCommand("stp_areas_add", sql, transaction))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        //cmd.Parameters.Add(new SqlParameter("@idArea", idArea));
+                        cmd.Parameters.Add(new SqlParameter("@nombre", "Willy10"));//evitar la vunerabilidad sql injection vidio 5
+                        cmd.Parameters.Add(new SqlParameter("@descripcion", "Willy10"));
+                        cmd.ExecuteNonQuery();
+                        //DataTable dt = new DataTable();
+                        //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        //sql.Open();
+
+                        //da.Fill(dt);
+                        //dataGridView1.DataSource = dt;
+                        //SqlDataReader lector;
+                        //lector = cmd.ExecuteReader();
+                    }
+                    transaction.Commit();
+
+                }
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+
+            Console.Read();
+
+            */
+            /*
+                        //ACTUALIZAR
+                        try
+                        {
+                            var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+                            //var nombre = "prueba addTransaction";
+                            //var descripcion = "descripcion";
+
+                            //var query = @"select * from Areas where idArea = @idArea";
+
+                            using (SqlConnection sql = new SqlConnection(connectionString))
+                            {
+                                sql.Open();
+
+                                using (SqlCommand cmd = new SqlCommand("stp_areas_update", sql))
+                                {
+                                    cmd.CommandType = CommandType.StoredProcedure;
+                                    cmd.Parameters.Add(new SqlParameter("@idArea", 56));
+                                    cmd.Parameters.Add(new SqlParameter("@nombre", "Willyy15"));//evitar la vunerabilidad sql injection vidio 5
+                                    cmd.Parameters.Add(new SqlParameter("@descripcion", "Willyy15"));
+                                    cmd.ExecuteNonQuery();
+                                    //DataTable dt = new DataTable();
+                                    //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                                    //sql.Open();
+
+                                    //da.Fill(dt);
+                                    //dataGridView1.DataSource = dt;
+                                    //SqlDataReader lector;
+                                    //lector = cmd.ExecuteReader();
+                                }
+
+
+
+                                var transaction = sql.BeginTransaction();
+                                using (SqlCommand cmd = new SqlCommand("stp_areas_update", sql, transaction))
+                                {
+                                    cmd.CommandType = CommandType.StoredProcedure;
+                                    cmd.Parameters.Add(new SqlParameter("@idArea", 57));
+                                    cmd.Parameters.Add(new SqlParameter("@nombre", "Willy16"));//evitar la vunerabilidad sql injection vidio 5
+                                    cmd.Parameters.Add(new SqlParameter("@descripcion", "Willy16"));
+                                    cmd.ExecuteNonQuery();
+                                    //DataTable dt = new DataTable();
+                                    //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                                    //sql.Open();
+
+                                    //da.Fill(dt);
+                                    //dataGridView1.DataSource = dt;
+                                    //SqlDataReader lector;
+                                    //lector = cmd.ExecuteReader();
+                                }
+                                //throw new ApplicationException("Ooops Error");
+
+
+                                using (SqlCommand cmd = new SqlCommand("stp_areas_update", sql, transaction))
+                                {
+                                    cmd.CommandType = CommandType.StoredProcedure;
+                                    cmd.Parameters.Add(new SqlParameter("@idArea", 58));
+                                    cmd.Parameters.Add(new SqlParameter("@nombre", "Willy17"));//evitar la vunerabilidad sql injection vidio 5
+                                    cmd.Parameters.Add(new SqlParameter("@descripcion", "Willy17"));
+                                    cmd.ExecuteNonQuery();
+                                    //DataTable dt = new DataTable();
+                                    //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                                    //sql.Open();
+
+                                    //da.Fill(dt);
+                                    //dataGridView1.DataSource = dt;
+                                    //SqlDataReader lector;
+                                    //lector = cmd.ExecuteReader();
+                                }
+                                transaction.Commit();
+
+                            }
+                        }
+                        catch (ApplicationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+
+
+                        Console.Read();
+            
+             */
+
+            
+            
+
+
+            /*TRIGGERS*/
+            /*Trigger 1 - Sucursal*/
+            try
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+                using (SqlConnection sql = new SqlConnection(connectionString))
+                {
+                    sql.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("stp_sucursales_add", sql))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add(new SqlParameter("@nombre", "Jair"));//evitar la vunerabilidad sql injection vidio 5
+                        cmd.Parameters.Add(new SqlParameter("@direccion", "Purisima Del Rincon"));
+                        cmd.Parameters.Add(new SqlParameter("@telefono", "4776512345"));
+                        cmd.ExecuteNonQuery();
+                        
+                    }
+                }
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+
+
+
+
+            /*Trigger 2 - Area*/
+            try
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+                using (SqlConnection sql = new SqlConnection(connectionString))
+                {
+                    sql.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("stp_areas_add", sql))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;                      
+                        cmd.Parameters.Add(new SqlParameter("@nombre", "Willy Nuevo"));//evitar la vunerabilidad sql injection vidio 5
+                        cmd.Parameters.Add(new SqlParameter("@descripcion", "Willy Nuevo"));
+                        cmd.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+
+
+
+            /*Trigger 3 - Socios*/
+            try
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+
+                using (SqlConnection sql = new SqlConnection(connectionString))
+                {
+                    sql.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("stp_socios_add", sql))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add(new SqlParameter("@nombre", "Jair"));//evitar la vunerabilidad sql injection vidio 5
+                        cmd.Parameters.Add(new SqlParameter("@apellido", "Montalvo"));
+                        cmd.Parameters.Add(new SqlParameter("@edad", "(SELECT FLOOR(RAND()*(50-18)+18))")); //Generamos una edad aleatoria
+                        cmd.Parameters.Add(new SqlParameter("@direccion", "Leon, Guanajuato"));
+                        cmd.Parameters.Add(new SqlParameter("@idSucursal", "2"));
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+
+        }
+
+
+
+    }
+}
